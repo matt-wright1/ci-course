ccc26a08c3b07c36181c6b1e80cee54f1347f851
Matthew Wright <matthew.wright@env-res.ox.ac.uk>
2021-10-18 15:26:37 +0100

    Updated testing for full coverage

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -14,6 +14,9 @@ class TestFunctionality(unittest.TestCase):
         self.assertEqual(ci_course.minimum(1.2, 2.3), 1.2)
         self.assertEqual(ci_course.minimum(-1.2, -3), -3)
 
+    def test_final_line(self):
+        self.assertEqual(ci_course.minimum('hello'), None)
+
 
 if __name__ == '__main__':
     unittest.main()
